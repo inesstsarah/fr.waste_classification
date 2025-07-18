df530517e3b93ba20cfa27ecf4fb67f32892e327
inescadit <ines.siti@cadit.com>
2025-07-18 15:52:48 +0700

    Add YOLO model and add camera to streamlit demo

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,19 +1,36 @@
 import streamlit as st
 import cv2
 import numpy as np
+from ultralytics import YOLO 
+import supervision as sv
+
+model_path = "MODEL_PATH"
+model_path = "yolo11n-seg.pt"
+# Define YOLO model being used
+model = YOLO(model_path)
+
+st.title("Trash Detection and Classification App")
 
 img_file_buffer = st.camera_input("Take a picture")
-
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    results = model(cv2_img)
+    detections = sv.Detections.from_ultralytics(results[0])
+
+    mask_annotator = sv.MaskAnnotator()
+    annotated_frame = mask_annotator.annotate(
+        scene=cv2_img.copy(),
+        detections=detections
+    )
+    label_annotator = sv.LabelAnnotator(text_position=sv.Position.CENTER)
+    annotated_frame = label_annotator.annotate(
+        scene=annotated_frame.copy(),
+        detections=detections
+    )
+
 
     # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
-
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
-    st.image(cv2_img, caption="Image from photo")
+    st.header("Trash Classification", divider="gray")
+    st.image(annotated_frame, caption="Segmentation and Classification of Trash")
